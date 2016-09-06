fd674e378d54b3598901992749bb4db178533d79
brich200 <brycerich@lawn-143-215-118-204.lawn.gatech.edu>
2016-09-06 18:27:26 -0400

    gitignore added

---
+++ reactive-stocks/target/scala-2.11/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,16 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/brycerich/Desktop/Fall2016/ECE4012/SeniorDesign/RamblinTek/reactive-stocks/conf/routes
+// @DATE:Tue Sep 06 18:15:00 EDT 2016
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
